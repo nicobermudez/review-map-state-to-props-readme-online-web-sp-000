047a5f2e78f0fbe647deb35e1dd657e48b516c51
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-02 21:58:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 const store = createStore(
-  shoppingListItemReducer, 
+  shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
